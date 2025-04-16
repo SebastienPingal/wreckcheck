90833a79aff695654997de69f76f1af0649d840e
Sebastien <sebastien.pingal@gmail.com>
2025-04-16 15:45:17 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  srcDir: "src/",
+  css: ["~/assets/css/tailwind.css"],
+
+  modules: ["shadcn-nuxt"],
+  shadcn: {
+    prefix: "S",
+    componentDir: "components/ui",
+  },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  routeRules: {
+    '/api/**': {
+      proxy: process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api/**" : "/api/**",
+    },
+    '/docs': {
+      proxy: "http://127.0.0.1:8000/docs",
+    },
+    '/openapi.json': {
+      proxy: "http://127.0.0.1:8000/openapi.json",
+    }
+  },
+
+  nitro: {
+    vercel: {
+      config: {
+        routes: [{
+          "src": "/api/(.*)",
+          "dest": "api/index.py"
+        }]
+      }
+    }
+  }
+})
